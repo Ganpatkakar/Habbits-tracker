be136642d71f2f2a4132d7514776a28fda833cfd
Ganpatkakar <kakarganpat@gmail.com>
2020-12-24 15:40:44 +0530

    Add active edit state Add delete list method logic Add enable edit method logic Add show lists logic

--- screens/home.js
+++ screens/home.js
@@ -1,34 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import {
-  SafeAreaView, StatusBar, StyleSheet
-} from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import HomeHeader from '../src/habbits/homeHeader';
 import ShowHabbits from '../src/habbits/showHabbits';
+import ShowDeletableLists from '../src/habbits/showDeletableLists';
 
 export default function Home() {
-  const [lists, setList] = useState([]);
+  const [lists, setList] = useState(['m1', 'm2']);
+  const [isActiveEdit, setActiveEdit] = useState(false);
   useEffect(() => {
-    getLists();
-  });
-  const getLists = () => {
-    const dummyLists = [
-      { title: 'meditation', habbitsChain: ['true', 'true', false] }
-    ];
-    setList(dummyLists);
+    if (!lists.length) {
+      setActiveEdit(false);
+    }
+  }, [lists]);
+  const enableEdit = () => {
+    setActiveEdit(!isActiveEdit);
   };
-  const editList = () => {
-
+  const addList = () => {};
+  const deleteLists = (index) => {
+    const newList = lists.filter((l, i) => i !== index);
+    setList(newList);
   };
-  const addList = () => {
-
-  };
-  const updateList = () => {
-
-  };
+  const updateList = () => {};
+  const showLists = isActiveEdit ? (
+    <ShowDeletableLists lists={lists} deleteLists={deleteLists} />
+  ) : (
+    <ShowHabbits lists={lists} updateList={updateList} />
+  );
   return (
     <SafeAreaView style={styles.container}>
-      <HomeHeader editList={editList} addList={addList} />
-      <ShowHabbits lists={lists} updateList={updateList} />
+      <HomeHeader
+        listsLength={lists.length}
+        enableEdit={enableEdit}
+        addList={addList}
+        isActiveEdit={isActiveEdit}
+      />
+      {showLists}
     </SafeAreaView>
   );
 }
@@ -36,6 +42,6 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight
-  }
+    marginTop: StatusBar.currentHeight,
+  },
 });
