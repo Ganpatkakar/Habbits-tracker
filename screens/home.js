bc955866e897a5df7575042dd5647ebe78e8a8f9
Ganpatkakar <kakarganpat@gmail.com>
2020-12-25 16:16:23 +0530

    Updated home screen to stack navigation for habbits

--- screens/home.js
+++ screens/home.js
@@ -1,47 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import HomeHeader from '../src/habbits/homeHeader';
-import ShowHabbits from '../src/habbits/showHabbits';
-import ShowDeletableLists from '../src/habbits/showDeletableLists';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HabbitsScreen from '../src/habbits/habbitScreen';
+import AddHabbitScreen from '../src/habbits/addHabbit/addHabbitScreen';
+import TipsScreen from '../src/habbits/addHabbit/tips/tipsScreen';
+import RepeatScreen from '../src/habbits/addHabbit/repeat/repeatScreen';
+import ReminderScreen from '../src/habbits/addHabbit/reminder/reminderScreen';
+import HabbitDetailsScreen from '../src/habbits/details/habbitDetailsScreen';
+
+const Stack = createStackNavigator();
 
 export default function Home() {
-  const [lists, setList] = useState(['m1', 'm2']);
-  const [isActiveEdit, setActiveEdit] = useState(false);
-  useEffect(() => {
-    if (!lists.length) {
-      setActiveEdit(false);
-    }
-  }, [lists]);
-  const enableEdit = () => {
-    setActiveEdit(!isActiveEdit);
-  };
-  const addList = () => {};
-  const deleteLists = (index) => {
-    const newList = lists.filter((l, i) => i !== index);
-    setList(newList);
-  };
-  const updateList = () => {};
-  const showLists = isActiveEdit ? (
-    <ShowDeletableLists lists={lists} deleteLists={deleteLists} />
-  ) : (
-    <ShowHabbits lists={lists} updateList={updateList} />
-  );
   return (
-    <SafeAreaView style={styles.container}>
-      <HomeHeader
-        listsLength={lists.length}
-        enableEdit={enableEdit}
-        addList={addList}
-        isActiveEdit={isActiveEdit}
-      />
-      {showLists}
-    </SafeAreaView>
+    <NavigationContainer independent>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Habbits"
+          component={HabbitsScreen}
+        />
+        <Stack.Screen
+          options={{
+            headerBackTitle: 'Cancel',
+            headerTitle: 'New Habbit',
+          }}
+          name="AddHabbits"
+          component={AddHabbitScreen}
+        />
+        <Stack.Screen
+          options={{
+            headerBackTitle: 'Back',
+            headerTitle: 'Details',
+          }}
+          name="HabbitDetails"
+          component={HabbitDetailsScreen}
+        />
+        <Stack.Screen
+          options={{ headerBackTitle: 'Back', headerTitle: 'Tips' }}
+          name="Tips"
+          component={TipsScreen}
+        />
+        <Stack.Screen
+          options={{ headerBackTitle: 'Back', headerTitle: 'Repeat' }}
+          name="Repeat"
+          component={RepeatScreen}
+        />
+        <Stack.Screen
+          options={{ headerBackTitle: 'Back', headerTitle: 'Reminder' }}
+          name="Reminder"
+          component={ReminderScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-});
