f13efbd1cde37ad60e390000b49cd013c604aed9
Ganpatkakar <kakarganpat@gmail.com>
2020-12-25 16:17:00 +0530

    Added habbits screen on home page to show habbits and its operations

---
+++ src/habbits/habbitScreen.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import HabbitsHeader from './habbitsComponents/habbitsHeader';
+import ShowHabbits from './habbitsComponents/showHabbits';
+import ShowDeletableLists from './habbitsComponents/showDeletableLists';
+
+export default function HabbitsScreen(props) {
+  const { navigation } = props;
+  const [lists, setList] = useState([{
+    title: 'm1',
+    dates: { '2020-12-25': true, '2020-12-24': true, '2020-12-20': true },
+  },
+  {
+    title: 'm2',
+    dates: { '2020-12-21': true, '2020-12-22': true, '2020-12-19': true },
+  }]);
+  const [isActiveEdit, setActiveEdit] = useState(false);
+
+  useEffect(() => {
+    if (!lists.length) {
+      setActiveEdit(false);
+    }
+  }, [lists]);
+  const enableEdit = () => {
+    setActiveEdit(!isActiveEdit);
+  };
+  const addList = () => {
+    navigation.navigate('AddHabbits');
+  };
+  const deleteLists = (index) => {
+    const newList = lists.filter((l, i) => i !== index);
+    setList(newList);
+  };
+  const updateList = () => {};
+  const showLists = isActiveEdit ? (
+    <ShowDeletableLists lists={lists} deleteLists={deleteLists} />
+  ) : (
+    <ShowHabbits navigation={navigation} lists={lists} updateList={updateList} />
+  );
+  return (
+    <SafeAreaView style={styles.container}>
+      <HabbitsHeader
+        listsLength={lists.length}
+        enableEdit={enableEdit}
+        addList={addList}
+        isActiveEdit={isActiveEdit}
+      />
+      {showLists}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+});
