1402b3b2668bb91a0ab7603c3268df23760a89c4
Ganpatkakar <kakarganpat@gmail.com>
2020-12-25 22:54:55 +0530

    Load marked dates with custom styles

--- src/habbits/details/habbitDetailsScreen.js
+++ src/habbits/details/habbitDetailsScreen.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { CalendarList } from 'react-native-calendars';
+import { useState, useEffect } from 'react/cjs/react.development';
 
-export default function HabbitDetailsScreen() {
+export default function HabbitDetailsScreen({ route }) {
+  const { dates } = route.params;
+  const [markedDates, setMarkedDates] = useState({});
+  useEffect(() => {
+    setMarkedDates(loadMarkedDates());
+  }, [dates]);
+
+  const loadMarkedDates = () => {
+    const newMarkedDates = {};
+    const keys = Object.keys(dates);
+    for (let i = 0; i < keys.length; i += 1) {
+      if (dates[keys[i]]) {
+        newMarkedDates[keys[i]] = {
+          customStyles: {
+            container: {
+              backgroundColor: 'green'
+            },
+            text: {
+              color: '#fff',
+              fontWeight: 'bold'
+            }
+          }
+        };
+      }
+    }
+    return { ...newMarkedDates };
+  };
   return (
     <View style={styles.calendarContainer}>
       <CalendarList
-        onVisibleMonthsChange={(months) => {
-          console.log('now these months are visible', months);
-        }}
-        onDayPress={(day) => { console.log('selected day', day) }}
+        markedDates={markedDates}
+        markingType="custom"
         // Max amount of months allowed to scroll to the past. Default = 50
         pastScrollRange={24}
         // Max amount of months allowed to scroll to the future. Default = 50
