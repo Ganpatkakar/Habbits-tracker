28c2bfe14e8613daa7b1a6fc0bffbbb91e21f4ae
Ganpatkakar <kakarganpat@gmail.com>
2020-12-31 15:36:03 +0530

    Updated todo lists

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+// TODO: Add Scheduled Reminder
+// TODO: Add Details page edit reminder time
+// TODO: Font Styling
+// TODO: Settings page styling design
+// TODO: Application details
 import React from 'react';
 // import HeaderComponent from "./components/Header";
 import MyTabs from './routes/routes';
