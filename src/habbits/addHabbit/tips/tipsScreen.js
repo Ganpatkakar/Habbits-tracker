4e857d366d185e310e3798db1d69c9b358c5176f
Ganpatkakar <kakarganpat@gmail.com>
2020-12-31 15:28:54 +0530

    Shop tips added tips list

--- src/habbits/addHabbit/tips/tipsScreen.js
+++ src/habbits/addHabbit/tips/tipsScreen.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, TouchableOpacity, FlatList
+} from 'react-native';
+import Separator from '../../../utils/seperator';
 
-export default function TipsScreen() {
+const tipsList = [
+  'Exercise', 'Eat More Vegetable', 'Limit Caffeine', 'Practice Yoga', 'Wake up early',
+  'Meditate', 'Journeling', 'Read', 'Study', 'Write code'
+];
+
+export default function TipsScreen({ navigation, route }) {
+  const { selectTips } = route.params;
+  const navigateBack = (val) => {
+    selectTips(val);
+    navigation.goBack();
+  };
+  const renderItem = ({ item }) => {
+    return (
+      <View key={item}>
+        <TouchableOpacity onPress={() => navigateBack(item)}>
+          <Text>{item}</Text>
+        </TouchableOpacity>
+        <Separator />
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Tips screen</Text>
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{ padding: 10 }}>
+      <Separator />
+      <FlatList
+        data={tipsList}
+        renderItem={renderItem}
+        keyExtractor={(item) => item}
+      />
+
     </View>
-  )
+  );
 }
