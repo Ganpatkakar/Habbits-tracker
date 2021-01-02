721067f100da55f176a1c8f7368e9520b9cc338d
Ganpatkakar <kakarganpat@gmail.com>
2021-01-02 15:31:15 +0530

    Add alert for delete operation include loading state for async storage include async storage for storing habits list in local

--- src/habits/habitScreen.js
+++ src/habits/habitScreen.js
@@ -1,68 +1,59 @@
 /* eslint-disable global-require */
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import {
+  SafeAreaView, StatusBar, StyleSheet, Text, View, Alert
+} from 'react-native';
 import { Audio } from 'expo-av';
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 import HabitsHeader from './habitsComponents/habitsHeader';
 import ShowHabits from './habitsComponents/showHabits';
 import ShowDeletableLists from './habitsComponents/showDeletableLists';
+import StorageService from '../utils/storageService';
+import { STORAGE_LISTS } from '../utils/commonStrings';
+import stylesCommon from '../utils/custom-text-style';
 
 export default function HabitsScreen(props) {
   const { navigation } = props;
-  const [lists, setList] = useState([{
-    id: uuidv4(),
-    title: 'Meditation',
-    dates: { '2020-12-25': true, '2020-12-24': true, '2020-12-20': true },
-  },
-  {
-    id: uuidv4(),
-    title: 'm2',
-    dates: { '2020-12-21': true, '2020-12-22': true, '2020-12-19': true },
-  },
-  {
-    dates: {
-      '2020-12-29': true,
-      '2020-12-30': true,
-      '2020-12-31': true,
-    },
-    id: '66942479-eff5-47ce-9be5-f42c74cfd257',
-    reminderTime: {
-      hour: 12,
-      minute: 3,
-    },
-    scheduleNotificationId: '4ab1f79f-9304-4145-81cd-fc9d0f71c60b',
-    title: 'Write code'
-  }
-  ]);
+  const [isLoading, setLoading] = useState(true);
+  const [lists, setList] = useState([]);
   const [isActiveEdit, setActiveEdit] = useState(false);
   let selectSoundObj = {};
   let deselectSoundObj = {};
+  Audio.setAudioModeAsync({
+    allowsRecordingIOS: false,
+    interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+    playsInSilentModeIOS: true,
+    interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+    shouldDuckAndroid: true,
+    staysActiveInBackground: true,
+    playThroughEarpieceAndroid: true
+  });
+  selectSoundObj = new Audio.Sound();
+  deselectSoundObj = new Audio.Sound();
+  const status = {
+    shouldPlay: false
+  };
+  // eslint-disable-next-line global-require
+  selectSoundObj.loadAsync(require('../../assets/audio/select.mp3'), status, false);
+  // eslint-disable-next-line global-require
+  deselectSoundObj.loadAsync(require('../../assets/audio/deselect.mp3'), status, false);
+
+  useEffect(() => {
+    StorageService.getJsonData(STORAGE_LISTS, getStorageCallbak, []);
+  }, []);
 
   useEffect(() => {
     if (!lists.length) {
       setActiveEdit(false);
     }
-    // console.log(lists);
-    Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-      playsInSilentModeIOS: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
-      shouldDuckAndroid: true,
-      staysActiveInBackground: true,
-      playThroughEarpieceAndroid: true
-    });
-    selectSoundObj = new Audio.Sound();
-    deselectSoundObj = new Audio.Sound();
-    const status = {
-      shouldPlay: false
-    };
-    // eslint-disable-next-line global-require
-    selectSoundObj.loadAsync(require('../../assets/audio/select.mp3'), status, false);
-    // eslint-disable-next-line global-require
-    deselectSoundObj.loadAsync(require('../../assets/audio/deselect.mp3'), status, false);
+    StorageService.storeJsonData(STORAGE_LISTS, lists);
   }, [lists]);
+
+  const getStorageCallbak = (value = []) => {
+    setLoading(false);
+    setList(value);
+  };
 
   const selectSound = () => {
     selectSoundObj.playAsync();
@@ -81,30 +72,54 @@ export default function HabitsScreen(props) {
       list.dates[day] = true;
       deselectSound();
     }
-    setList([...lists]);
+    // setList([...lists]);
+    getStorageCallbak([...lists]);
   };
 
   const enableEdit = () => {
     setActiveEdit(!isActiveEdit);
   };
+
   const addList = () => {
     navigation.navigate('AddHabits', { addNewHabit });
   };
+
   const addNewHabit = (data = {}) => {
     // eslint-disable-next-line no-param-reassign
     data.id = uuidv4();
     lists.push(data);
-    setList([...lists]);
+    // setList([...lists]);
+    getStorageCallbak([...lists]);
   };
+
   const deleteLists = (id) => {
-    const newList = lists.filter((l) => l.id !== id);
-    setList(newList);
+
+    Alert.alert(
+      'Delete',
+      'My Habit',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel'
+        },
+        {
+          text: 'OK',
+          onPress: () => {
+            const newList = lists.filter((l) => l.id !== id);
+            // setList(newList);
+            getStorageCallbak([...newList]);
+          }
+        }
+      ],
+      { cancelable: false }
+    );
   };
 
   const editCompleteHabit = (list) => {
-    console.log('newlist', list);
+    // console.log('newlist', list);
     const newList = lists.map((li) => (li.id === list.id ? list : li));
-    setList([...newList]);
+    // setList([...newList]);
+    getStorageCallbak([...newList]);
   };
 
   const showLists = isActiveEdit ? (
@@ -118,6 +133,12 @@ export default function HabitsScreen(props) {
     />
   );
 
+  const loadingComponent = (
+    <View style={styles.habitsContainer}>
+      <Text style={stylesCommon.normalText}>Loading ...</Text>
+    </View>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <HabitsHeader
@@ -126,7 +147,9 @@ export default function HabitsScreen(props) {
         addList={addList}
         isActiveEdit={isActiveEdit}
       />
-      {showLists}
+      {
+        isLoading ? loadingComponent : showLists
+      }
     </SafeAreaView>
   );
 }
@@ -135,5 +158,8 @@ const styles = StyleSheet.create({
   container: {
     marginTop: StatusBar.currentHeight,
     fontSize: 20
+  },
+  habitsContainer: {
+    padding: 5
   },
 });
